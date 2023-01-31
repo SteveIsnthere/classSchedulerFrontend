dae6460b95f2a392d934132f2a256c3552254e5a
priva <yuyustevejobs@icloud.com>
2023-01-30 22:55:31 -0800

    update

--- src/app/dashboard/admin-management/add-member/add-member.component.ts
+++ src/app/dashboard/admin-management/add-member/add-member.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
+import {Member} from "../../../data/models/Member";
+import {DataService} from "../../../data/data.service";
 
 @Component({
   selector: 'app-add-member',
@@ -7,14 +9,53 @@ import {FormBuilder, Validators} from '@angular/forms';
   styleUrls: ['./add-member.component.css']
 })
 export class AddMemberComponent {
-  firstFormGroup = this._formBuilder.group({
-    nameCtrl: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
-  name = this.firstFormGroup.get('nameCtrl');
+
+  name:string = '';
+  nickname:string = '';
+  email: string = '';
+  phone: string = '';
+  password: string = '';
+  isTeacher: boolean = false;
   isEditable = false;
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(private _formBuilder: FormBuilder, private dataService: DataService) {}
+  firstFormGroup = this._formBuilder.group({
+    nameCtrl: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
+    nicknameCtrl: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
+    passwordCtrl: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+  });
+  secondFormGroup = this._formBuilder.group({
+    emailCtrl: ['', [Validators.required, Validators.email]],
+    phoneCtrl: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+  });
+
+  isNickNameTaken() {
+    return false;
+  }
+
+  submit() {
+    if (this.isNickNameTaken()){
+      alert('Nickname is taken');
+      return;
+    }
+    let member = {
+      _id: '',
+      name: this.name,
+      nickname: this.nickname,
+      email: this.email,
+      phone: this.phone,
+      password: this.password,
+      isTeacher: this.isTeacher,
+      about: '',
+      noteToAdmin: '',
+      courses: [],
+      unableTimes: [],
+      preferredTimes: []
+    }
+    this.dataService.createMember(member);
+  }
+
+
+  closeDialog() {
+  }
 }
--- src/app/reusable-components/relation-cell/relation-cell.component.ts
+++ src/app/reusable-components/relation-cell/relation-cell.component.ts
@@ -1,23 +1,56 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {dummyRelation, Relation} from "../../data/models/Relation";
 import {RelationDetailDialogComponent} from "./relation-detail-dialog/relation-detail-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
+import {Member} from "../../data/models/Member";
+import {DataService} from "../../data/data.service";
 
 @Component({
   selector: 'app-relation-cell',
   templateUrl: './relation-cell.component.html',
   styleUrls: ['./relation-cell.component.css']
 })
-export class RelationCellComponent {
+export class RelationCellComponent implements OnInit {
   @Input() relation: Relation = dummyRelation();
   @Input() isTeacher: boolean = true;
+  relatedMember: null | Member = null;
 
-  constructor(private dialog: MatDialog) {
+  notShowDetail: boolean = false;
+
+  constructor(private dialog: MatDialog, private dataService: DataService) {
   }
 
+
   showBottomSheet() {
+    if (this.notShowDetail) {
+      return;
+    }
     this.dialog.open(RelationDetailDialogComponent, {
       data: this.relation
     });
+  }
+
+  preventShowDetail() {
+    this.notShowDetail = true;
+    // set a timer to reset the flag
+    setTimeout(() => {
+      this.notShowDetail = false;
+    }, 30);
+  }
+
+  ngOnInit(): void {
+    let relatedMemberNickName = ''
+    if (this.isTeacher) {
+      relatedMemberNickName = this.relation.student;
+    } else {
+      relatedMemberNickName = this.relation.teacher;
+    }
+    console.log("relatedMember: " + relatedMemberNickName)
+    this.dataService.getMember(relatedMemberNickName).subscribe(
+      (member) => {
+        this.relatedMember = member;
+        console.log("relatedMember: " + this.relatedMember)
+      }
+    )
   }
 }
