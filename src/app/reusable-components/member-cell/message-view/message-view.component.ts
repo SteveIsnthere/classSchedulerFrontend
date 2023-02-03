a738225ad075182fcc183169742061bf4c0eb1d3
stevewang <privacyalwaysfirst@proton.me>
2023-02-03 02:37:47 -0800

    damn

--- src/app/reusable-components/member-cell/message-view/message-view.component.ts
+++ src/app/reusable-components/member-cell/message-view/message-view.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 import {DataService} from "../../../data/data.service";
+import {AuthService} from "../../../data/auth.service";
 
 @Component({
   selector: 'app-message-view',
@@ -8,9 +9,23 @@ import {DataService} from "../../../data/data.service";
   styleUrls: ['./message-view.component.css']
 })
 export class MessageViewComponent {
+  message: string = '';
 
-    constructor(@Inject(MAT_DIALOG_DATA) public receiverNickName: string, private dataService: DataService) {
+  constructor(@Inject(MAT_DIALOG_DATA) public receiverNickName: string, private dataService: DataService, private authService: AuthService) {
+  }
 
+  send() {
+    if (this.message.trim() === '') {
+      return;
+    }
+    let newMessage = {
+      _id: '',
+      senderNickName: this.authService.self!.nickname,
+      receiverNickName: this.receiverNickName,
+      time: new Date().toISOString(),
+      content: this.message
     }
 
+    this.dataService.postMessage(newMessage);
+  }
 }
